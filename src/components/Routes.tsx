9e1bb6a082342a55a9af0b766b041f7aa9523fdb
Matthew Francis <mattfrancis888@gmail.com>
2021-05-08 14:39:40 -0400

    Added header.

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
-
-// import Header from "./Header";
+import Header from "./Header";
 const Routes: React.FC<{}> = () => {
     return (
         <React.Fragment>
-            {/* <Header /> */}
+            <Header />
             <Switch>
                 <Route path="/" exact component={Home} />
             </Switch>
